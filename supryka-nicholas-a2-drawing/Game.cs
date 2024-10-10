a8663ee405b4dbe32d0cb43d6f50ef369dff4c6f
nicholassumisu@gmail.com <nicholassumisu@gmail.com>
2024-10-10 11:03:37 -0400

    Add first squares draw functions

--- supryka-nicholas-a2-drawing/Game.cs
+++ supryka-nicholas-a2-drawing/Game.cs
@@ -12,6 +12,16 @@ namespace Game10003
     {
         // Place your variables here:
         Color beige = new Color(190, 175, 135);
+        Color grassGreen = new Color(90, 190, 85);
+        Vector2 squarePos1 = new Vector2(2, 2);
+        Vector2 squarePos2 = new Vector2(132, 2);
+        Vector2 squarePos3 = new Vector2(264, 2);
+        Vector2 squarePos4 = new Vector2(2, 132);
+        Vector2 squarePos5 = new Vector2(132, 132);
+        Vector2 squarePos6 = new Vector2(264, 132);
+        Vector2 squarePos7 = new Vector2(2, 264);
+        Vector2 squarePos8 = new Vector2(132, 264);
+        Vector2 squarePos9 = new Vector2(264, 264);
 
         /// <summary>
         ///     Setup runs once before the game loop begins.
@@ -27,7 +37,56 @@ namespace Game10003
         /// </summary>
         public void Update()
         {
-            Window.ClearBackground(beige);
+            Window.ClearBackground(grassGreen);
+
+            Draw.LineSize = 0;
+            Draw.FillColor = beige;
+            //Top Left
+            if ((Input.GetMouseX() > 132) || (Input.GetMouseY() > 132))
+            {
+                Draw.Square(squarePos1, 132);
+            }
+            //Top Middle
+            if ((Input.GetMouseX() > 132) || (Input.GetMouseY() > 132))
+            {
+                Draw.Square(squarePos2, 132);
+            }
+            //Top Right
+            if ((Input.GetMouseX() > 132) || (Input.GetMouseY() > 132))
+            {
+                Draw.Square(squarePos3, 132);
+            }
+            //Middle Left
+            if ((Input.GetMouseX() > 132) || (Input.GetMouseY() > 132))
+            {
+                Draw.Square(squarePos4, 132);
+            }
+            //Middle Middle
+            if ((Input.GetMouseX() > 132) || (Input.GetMouseY() > 132))
+            {
+                Draw.Square(squarePos5, 132);
+            }
+            //Middle Right
+            if ((Input.GetMouseX() > 132) || (Input.GetMouseY() > 132))
+            {
+                Draw.Square(squarePos6, 132);
+            }
+            //Bottom Left
+            if ((Input.GetMouseX() > 132) || (Input.GetMouseY() > 132))
+            {
+                Draw.Square(squarePos7, 132);
+            }
+            //Bottom Middle
+            if ((Input.GetMouseX() > 132) || (Input.GetMouseY() > 132))
+            {
+                Draw.Square(squarePos8, 132);
+            }
+            //Bottom Right
+            if ((Input.GetMouseX() < 264) || (Input.GetMouseY() < 264))
+            {
+                Draw.Square(squarePos9, 132);
+            }
+
         }
     }
 }
